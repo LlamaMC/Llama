092d860ff5b706424c23a23a5b1a0ab3b84fe302
Tom <eztxmmc@hotmail.com>
2025-12-21 22:06:03 +0100

    fixed issue

--- llama/src/main/java/com/llamamc/llama/example/command/TestCommand2.java
+++ llama/src/main/java/com/llamamc/llama/example/command/TestCommand2.java
@@ -1,6 +1,6 @@
 package com.llamamc.llama.example.command;
 
-import com.llamamc.llamaapi.command.Arg;
+import com.llamamc.llamaapi.command.Argument;
 import com.llamamc.llamaapi.command.ArgumentType;
 import com.llamamc.llamaapi.command.Command;
 import com.llamamc.llamaapi.command.ICommandContext;
@@ -9,8 +9,7 @@ import com.llamamc.llamaapi.player.IPlayer;
 public class TestCommand2 {
 
     @Command(name = "test")
-    public void test(ICommandContext context, @Arg(name = "player", type = ArgumentType.PLAYER_NAME, position = 0) IPlayer player) {
+    public void test(ICommandContext context, @Argument(name = "player", type = ArgumentType.PLAYER_NAME, position = 0) IPlayer player) {
 
     }
-
 }
